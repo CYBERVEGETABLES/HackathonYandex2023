48554c3ff4ce60f315f403541d79a329c3c41e45
AnnaVikj <114988685+AnnaVikj@users.noreply.github.com>
2023-03-22 22:56:06 +0700

    marks and week schedule

--- src/diary.py
+++ src/diary.py
@@ -1,8 +1,9 @@
 import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 from dotenv import load_dotenv
 
@@ -11,7 +12,8 @@ class DiaryNSO:
     def __init__(self, login: str, password: str):
         options = Options()
         # options.add_argument('-headless')
-        self.driver = webdriver.Firefox(options=options)
+        # self.driver = webdriver.Firefox(options=options)
+        self.driver = webdriver.Chrome(options=options)
         self.login = login
         self.password = password
 
@@ -26,6 +28,45 @@ class DiaryNSO:
 
         form.find_element(By.TAG_NAME, 'button').click()
 
+    def get_today_marks(self) -> str:
+        what = 'расписание на неделю'  # пользователь говорит что хочет, либо оценки, либо расписание :)
+        response = list()
+        time.sleep(2)
+        # TODO реализовать кнопку, чтобы переключала недели
+        self.driver.get('https://school.nso.ru/journal-app/u.931/week.1')
+        days = self.driver.find_elements(By.CLASS_NAME, 'dnevnik-day')
+        if 'оцен' in what and what.isalnum():
+            for i in days:
+                if '13.03' in i.text:  # здесь также рандомное число, но позже будет дата
+                    massiv = (i.text).split('\n')
+            for j in range(len(massiv)):
+                if len(massiv[j]) == 1 and massiv[j].isdigit():
+                    stroka = massiv[j - 1] + " " + massiv[j]
+                    response.append(stroka)
+            if len(response) != 0:
+                for i in response:
+                    print(i)
+            else:
+                print("В этот день у вас нет оценок")
+        elif 'расп' in what and 'недел' in what:
+            massiv = list()
+            for k in days:
+                massiv.append(k.text.split('\n'))
+            for t in range(len(massiv)):
+                response.append(massiv[t][0])
+                for w in range(len(massiv[t])):
+                    if len(massiv[t][w]) == 2 and massiv[t][w][1] == '.':
+                        if ':' in massiv[t][w + 1] and ('0' in massiv[t][w + 1] or '1' in massiv[t][w + 1]):
+                            stroka = massiv[t][w] + " " + massiv[t][w + 2]
+                        else:
+                            stroka = massiv[t][w] + " " + massiv[t][w + 1]
+                        response.append(stroka)
+            if len(response) != 0:
+                for i in response:
+                    print(i)
+            else:
+                print("У вас нет уроков!")
+
     def quit(self):
         self.driver.quit()
 
@@ -39,6 +80,7 @@ def main():
     )
 
     diary.auth()
+    print(diary.get_today_marks())
     input()
     diary.quit()
 
