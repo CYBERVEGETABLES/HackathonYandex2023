bbb18ffd216c35fe7ac60c4fd973c3e4559eea8d
Sergey Lebedev <ob430y@yandex.ru>
2023-03-23 02:11:36 +0700

    Patch auth() & Add get_next_day_schedule()

--- src/diary.py
+++ src/diary.py
@@ -1,9 +1,10 @@
 import os
 import time
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 from dotenv import load_dotenv
 
@@ -12,8 +13,7 @@ class DiaryNSO:
     def __init__(self, login: str, password: str):
         options = Options()
         # options.add_argument('-headless')
-        # self.driver = webdriver.Firefox(options=options)
-        self.driver = webdriver.Chrome(options=options)
+        self.driver = webdriver.Firefox(options=options)
         self.login = login
         self.password = password
 
@@ -21,55 +21,47 @@ class DiaryNSO:
         self.driver.get('https://school.nso.ru/authorize')
 
         form = self.driver.find_element(By.TAG_NAME, 'form')
-
         inputs = form.find_elements(By.TAG_NAME, 'input')
-        inputs[0].send_keys(self.login)
-        inputs[1].send_keys(self.password)
 
-        form.find_element(By.TAG_NAME, 'button').click()
+        while self.driver.current_url != 'https://school.nso.ru/journal-app':
+            inputs[0].send_keys(self.login)
+            inputs[1].send_keys(self.password)
 
-    def get_today_marks(self) -> str:
-        what = 'расписание на неделю'  # пользователь говорит что хочет, либо оценки, либо расписание :)
-        response = list()
-        time.sleep(2)
-        # TODO реализовать кнопку, чтобы переключала недели
-        self.driver.get('https://school.nso.ru/journal-app/u.931/week.1')
-        days = self.driver.find_elements(By.CLASS_NAME, 'dnevnik-day')
-        if 'оцен' in what and what.isalnum():
-            for i in days:
-                if '13.03' in i.text:  # здесь также рандомное число, но позже будет дата
-                    massiv = (i.text).split('\n')
-            for j in range(len(massiv)):
-                if len(massiv[j]) == 1 and massiv[j].isdigit():
-                    stroka = massiv[j - 1] + " " + massiv[j]
-                    response.append(stroka)
-            if len(response) != 0:
-                for i in response:
-                    print(i)
-            else:
-                print("В этот день у вас нет оценок")
-        elif 'расп' in what and 'недел' in what:
-            massiv = list()
-            for k in days:
-                massiv.append(k.text.split('\n'))
-            for t in range(len(massiv)):
-                response.append(massiv[t][0])
-                for w in range(len(massiv[t])):
-                    if len(massiv[t][w]) == 2 and massiv[t][w][1] == '.':
-                        if ':' in massiv[t][w + 1] and ('0' in massiv[t][w + 1] or '1' in massiv[t][w + 1]):
-                            stroka = massiv[t][w] + " " + massiv[t][w + 2]
-                        else:
-                            stroka = massiv[t][w] + " " + massiv[t][w + 1]
-                        response.append(stroka)
-            if len(response) != 0:
-                for i in response:
-                    print(i)
-            else:
-                print("У вас нет уроков!")
+            form.find_element(By.TAG_NAME, 'button').click()
+
+            time.sleep(1)
+
+    def get_next_day_schedule(self) -> str:
+        def get_tomorrow_date() -> str:
+            """ Returns tomorrow date in format '%d.%m' """
+            return f'{int(datetime.today().strftime("%d")) + 1}.{datetime.today().strftime("%m")}'
+
+        response = ''
+        tomorrow_date = get_tomorrow_date()
+
+        days = []
+        while len(days) == 0:
+            days = self.driver.find_elements(By.CLASS_NAME, 'dnevnik-day')
+
+        for day in days:
+            current_day_date = day.find_element(
+                By.CLASS_NAME, 'dnevnik-day__title'
+            ).text.split(', ')[1]
+
+            if current_day_date == tomorrow_date:
+                subjects = day.find_elements(By.CLASS_NAME, 'js-rt_licey-dnevnik-subject')
+
+                if len(subjects) == 0:
+                    return 'На завтра уроков не найдено'
+
+                response = 'Завтра у Вас будут следующие предметы: ' + \
+                           ', '.join([subject.text for subject in subjects])
+
+        return response
 
     def quit(self):
         self.driver.quit()
-#так ...
+
 
 def main():
     load_dotenv()
@@ -80,8 +72,7 @@ def main():
     )
 
     diary.auth()
-    print(diary.get_today_marks())
-    input()
+    print(diary.get_next_day_schedule())
     diary.quit()
 
 
